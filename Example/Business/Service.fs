d245171ef979dbb778023834f4cf3cc03be4c1d1
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-27 14:45:32 +0200

    ref

--- Example/Business/Service.fs
+++ Example/Business/Service.fs
@@ -1,15 +1,15 @@
 ﻿module Business.Service
     
-    open System
+open System
 
-    let create (insert : ``insert project``) : ``create project`` =
-        fun project -> async {
-            let id = Guid.NewGuid() |> ProjectId
-            do! (id, project) |> insert
-            return id
-        }
-    
-    let get (laod : ``load project``) : ``get project`` =
-        fun id -> async {
-            return! id |> laod
-        }
+let create (insert : ``insert project``) : ``create project`` =
+    fun project -> async {
+        let id = Guid.NewGuid() |> ProjectId
+        do! (id, project) |> insert
+        return id
+    }
+
+let get (laod : ``load project``) : ``get project`` =
+    fun id -> async {
+        return! id |> laod
+    }
