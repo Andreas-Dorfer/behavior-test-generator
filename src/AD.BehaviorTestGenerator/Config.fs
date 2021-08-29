b70d72aea5ef1cb68cb5910d8b237e5526d06813
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-29 11:43:18 +0200

    refactoring

--- src/AD.BehaviorTestGenerator/Config.fs
+++ src/AD.BehaviorTestGenerator/Config.fs
@@ -8,3 +8,14 @@ let classAttribute = "classAttribute"
 
 [<Literal>]
 let methodAttribute = "methodAttribute"
+
+let read getter =
+    name
+    |> getter
+    |> Seq.choose (fun (key, value : obj) ->
+        if key = classAttribute || key = methodAttribute then
+            match value with
+            | :? string as value -> Some (key, value)
+            | _ -> None
+        else None)
+    |> Map.ofSeq
--- src/AD.BehaviorTestGenerator/Generator.fs
+++ src/AD.BehaviorTestGenerator/Generator.fs
@@ -5,17 +5,6 @@ open Myriad.Core
 [<MyriadGenerator (Config.name)>]
 type Generator () =
 
-    let readConfig getter =
-        Config.name
-        |> getter
-        |> Seq.choose (fun (key, value : obj) ->
-            if key = Config.classAttribute || key = Config.methodAttribute then
-                match value with
-                | :? string as value -> Some (key, value)
-                | _ -> None
-            else None)
-        |> Map.ofSeq
-
     interface IMyriadGenerator with
 
         member _.ValidInputExtensions = seq { ".fs" }
@@ -25,5 +14,5 @@ type Generator () =
             if not System.Diagnostics.Debugger.IsAttached then
                 System.Diagnostics.Debugger.Launch() |> ignore
 #endif
-            let config = context.ConfigGetter |> readConfig
+            let config = context.ConfigGetter |> Config.read
             context.InputFilename |> Behavior.fromFile |> List.map (Test.create config)
